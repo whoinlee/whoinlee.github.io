8783d8b28ec276034f20f42e18a539c7e41352e5
WhoIN Lee <whoinlee@gmail.com>
2023-10-22 22:57:36 -0400

    style set up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,22 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter, Lato, Open_Sans } from 'next/font/google'
+import './styles/layout.scss'
 
-const inter = Inter({ subsets: ['latin'] })
+//-- fonts
+const inter = Inter({ subsets: ['latin'] });
+const lato = Lato({ 
+  subsets: ['latin'],
+  weight: ['400', '700', '900'],
+  variable: '--font-lato'
+ });
+const openSans = Open_Sans({ 
+  subsets: ['latin'],
+  variable: '--font-open-sans'
+ });
 
 export const metadata: Metadata = {
   title: 'i_&_UU+',
-  description: 'Whoin Lee\'s Portfolio site',
+  description: 'Whoin Lee\'s portfolio site',
 }
 
 export default function RootLayout({
@@ -16,7 +26,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      {/* <body className={inter.className}>{children}</body> */}
+      <body className={`${openSans.className} ${openSans.variable} ${lato.variable}`}>{children}</body>
     </html>
   )
 }
